c2db5193d8524ca7ba496db06bc5772e502deabd
danskode <149320369+danskode@users.noreply.github.com>
2025-10-15 14:56:48 +0200

    app.js scaffolding

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
 console.log("hello world");
+
+app.use(express.static("public"));
+
+// ================================= PAGES =====================================
+
+// ================================= APIS ======================================
+
+// ================================= SERVER INFO ===============================
+
+const PORT = Number(process.env.PORT) || 8080;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log("Server didn't start on port", PORT);
+  }
+
+  console.log("Server is running on port", PORT);
+});
